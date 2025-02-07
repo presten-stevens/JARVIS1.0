68b9115fe9a79ec3549b3ffa17ca2f504df4c70a
Benjamin Oakeson <oakesonbenjamin@gmail.com>
2025-02-07 08:30:48 -0700

    Adding input to the reflective questions and fixing the bug in the task.py file

--- task.py
+++ task.py
@@ -1,8 +1,7 @@
 class task:
-    def __init__(self, name, description):
+    def __init__(self, name, description, completed=False):
         self.name = name
         self.description = description
-        self.completed = False
 
     def __str__(self):
         return f"Task: {self.name}\nDescription: {self.description}\nCompleted: {self.completed}"
