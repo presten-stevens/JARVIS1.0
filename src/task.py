fbf3aef7f900d28b00b489421bd71b148891eb69
Joshua LeVar <joshlevar@gmail.com>
2025-02-17 11:03:09 -0700

    Implemented(task.py)

--- src/task.py
+++ src/task.py
@@ -1,7 +1,36 @@
-class task:
-    def __init__(self, name, description, completed=False):
+class Task:
+    def __init__(self, name: str, description: str, priority: int,
+                dueDate: str, category: str, completed: bool = False):
         self.name = name
         self.description = description
+        self.priority = priority
+        self.dueDate = dueDate
+        self.category = category
+        self.completed = completed
 
     def __str__(self):
-        return f"Task: {self.name}\nDescription: {self.description}\nCompleted: {self.completed}"
+        return (f"Name: {self.name}\n"
+            f"Description: {self.description}\n"
+            f"Priority: {self.priority}\n"
+            f"Due Date: {self.dueDate}\n"
+            f"Category: {self.category}\n"
+            f"Completed: {self.completed}\n"
+        )
+    
+    def setName(self, name):
+        self.name = name
+
+    def setDescription(self, description: str):
+        self.description = description
+
+    def setPriority(self, priority: int):
+        self.priority = priority
+
+    def setDueDate(self, dueDate: str):
+        self.dueDate = dueDate
+
+    def setCategory(self, category: str):
+        self.category = category
+
+    def setCompleted(self, completed: bool):
+        self.completed = completed
