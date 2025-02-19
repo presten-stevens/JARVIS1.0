f0972f5665700c9857af0af068235f7afadb9f2e
Benjamin Oakeson <oakesonbenjamin@gmail.com>
2025-02-18 17:57:22 -0700

    Made functioning task functions. Stored using dictionary

--- src/task.py
+++ src/task.py
@@ -1,7 +1,8 @@
-class task:
+class Task:
     def __init__(self, name, description, completed=False):
         self.name = name
         self.description = description
+        self.completed = completed
 
     def __str__(self):
-        return f"Task: {self.name}\nDescription: {self.description}\nCompleted: {self.completed}"
+        return f"Name: {self.name}\nDescription: {self.description}"
