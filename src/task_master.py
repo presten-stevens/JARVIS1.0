48d0551f48827c74dc81813ead57f12278fcf5ce
jlevar <joshlevar@gmail.com>
2025-02-20 10:46:49 -0700

    modified(editTask) with self.tasks being dict

--- src/task_master.py
+++ src/task_master.py
@@ -1,9 +1,8 @@
-from typing import List
 from task import Task
 
 class Taskmaster:
     def __init__(self):
-        self.tasks: List[Task] = []
+        self.tasks = {}
     
     # Add a task
     
@@ -15,19 +14,13 @@ class Taskmaster:
 
     # Load Tasks from JSON
 
-    def _getTask(self, task_name):
-        for task in self.tasks:
-            if task.name == task_name:
-                return task
-        return None
-
     # Edit Tasks
-    def editTask(self, task_name: str, target_var: str, revision: str):
-        task = self._getTask(task_name)
-
-        if not task:
-            raise Exception(f"Task '{task_name}' not found")
+    def editTask(self, task_id: int, target_var: str, revision: str):
+        if task_id not in self.tasks:
+            raise Exception(f"Task ID#'{task_id}' not found")
         
+        task = self.tasks[task_id]
+
         target_var = target_var.lower()
         if target_var not in vars(task).keys():
             raise Exception(f"Variable '{target_var}' not valid Task attribute")
